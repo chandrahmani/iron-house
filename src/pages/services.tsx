8b6936f9e2b56739286406f70770112dcdeb67e2
Chand Rahmani <chandrahmani254@gmail.com>
2023-08-25 18:57:25 +0530

    .

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -45,7 +45,7 @@ const services = () => {
           <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
             <img
               className="w-[10rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[35rem]"
-              src="/images/Training.jpg"
+              src="/images/training.jpg"
               alt=""
             />
           </div>
